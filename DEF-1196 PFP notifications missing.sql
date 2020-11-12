dfc4fe3cfa09ee7c5d4ab948c865c2460210b3c4
Vladimir Afanasenka <v.afanasenka@godeltech.com>
2020-11-12 15:38:00 +0000

    Update DEF-1196 PFP notifications missing.sql

--- DEF-1196 PFP notifications missing.sql
+++ DEF-1196 PFP notifications missing.sql
@@ -1,1 +1,69 @@
+--get secure messages from to
+SELECT *
+FROM [securemessage].[dbo].[TSecureMessage] m
+join [securemessage].[dbo].[TSecureMessageRecipient] r on m.SecureMessageId = r.SecureMessageId
+join [CRM].[dbo].[TContact] c ON m.SenderPartyId = c.CRMContactId
 
+where
+TenantId = 752
+and SenderPartyId = 12654941
+and PartyId = 25562990
+and c.RefContactType = 'E-Mail'
+order by m.[SecureMessageId]
+
+--get Client claims
+select * from
+[Membership].[dbo].[TClaim]
+where AccountId = 304924
+
+--get account and email info by email
+declare @Email VARCHAR(30) = 'lawrencebyrne@virginmedia.com'
+
+SELECT *
+FROM [Membership].[dbo].[TAccount]
+where Email like @Email
+
+SELECT *
+FROM [email].[dbo].[TEmailRecipient]
+where EmailAddress like @Email
+
+SELECT *
+  FROM [email].[dbo].[TEmailDelivery]
+  where Recipients like @Email
+
+SELECT *
+  FROM [email].[dbo].[TEmailSent]
+  where Destination like @Email
+
+--get Adviser's Account and claims
+SELECT *
+FROM [Membership].[dbo].[TAccount]
+where Email like 'frank.harewood@avtrinity.com'
+
+select * from
+[Membership].[dbo].[TClaim]
+where AccountId = 12364
+
+--get Clieny and Adviser user's data
+SELECT top 1000
+c.CRMContactId, c.PersonId, c.LastName, c.FirstName, c.DOB, c.Postcode, c.OriginalAdviserCRMId, c.CurrentAdviserCRMId, c.CurrentAdviserName,
+c.CRMContactType, c.IndClientId, c.InternalContactFG, c.RefServiceStatusId, c.MigrationRef, c.CreatedDate, c.ExternalReference, c.CampaignDataId,
+c.AdditionalRef, c.AdviserAssignedByUserId, c._OwnerId, c.FeeModelId, c.ServiceStatusStartDate, c.ClientTypeId, c.IsHeadOfFamilyGroup, c.FamilyGroupCreationDate,
+c.IsDeleted,
+u.UserId, u.Identifier, u.RefUserTypeId, u.Guid, u.Password as [UserPassword], u.PasswordSalt as [UserPasswordSalt], u.SyncPassword, u.ExpirePasswordOn,
+u.Email as [UserEmail], u.EmailConfirmed, u.Status, u.GroupId, u.Telephone, u.Reference,
+u.CanLogCases,
+a.AccountId, a.Subject, a.Username, a.Email as [AccountEmail], a.IsLoginAllowed, a.IsAccountVerified, a.Password as [AccountPassword], a.PasswordSalt as [AccountPasswordSalt],
+a.PasswordAlgorithm, a.PasswordChanged, a.LastLogin, a.LastFailedLogin, a.LastLockedOutDate, a.Created, a.LastUpdated, a.SyncDate,
+rut.RefUserTypeId, rut.Identifier, rut.Url, rut.ConcurrencyId
+--c.PersonId, u.CRMContactId, u.UserId, a.AccountId, u.Email, u.groupId, c.PersonId, a.Username, rut.Identifier, u.Guid
+ FROM crm..TCrmContact c
+ JOIN [administration].[dbo].[TUser] u on u.CRMContactId = c.CRMContactId
+ JOIN Membership..TAccount a on a.Subject = u.Guid
+ JOIN [administration].[dbo].[TRefUserType] rut on rut.RefUserTypeId = u.RefUserTypeId
+ Where
+c.IndClientId = 752
+and
+(
+c.CRMContactId = 25562990
+or c.CRMContactId = 12654941)
